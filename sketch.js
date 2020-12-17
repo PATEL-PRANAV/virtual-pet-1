a4c4bb7dac5e8d78530954e1bed2d448b5244cf0
PATEL-PRANAV <71432271+PATEL-PRANAV@users.noreply.github.com>
2020-12-16 22:22:23 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var dog, happyDog, database, foodS, foodStock;
+
+function preload()
+{
+  dogImage = loadImage("sprites/Dog.png")
+  happyDogImage = loadImage("sprites/happydog.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
